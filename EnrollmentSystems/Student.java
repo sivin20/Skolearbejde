536d1e59ef2a87b4aae95ffccacc6f807b3a07b6
sivin20 <71690233+sivin20@users.noreply.github.com>
2020-10-19 11:56:01 +0200

    Add files via upload

---
+++ EnrollmentSystems/Student.java
@@ -0,0 +1,14 @@
+
+public class Student
+{
+    String name;
+    int id;
+    Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+}
